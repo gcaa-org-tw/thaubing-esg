08ad8961eaffbf84c9356d95c4ec82870e3e46b7
Hsin <hsin@impactinstitute.com>
2021-08-26 20:18:59 +0200

    scrape consolidated financial reports

--- crawler/financial/settings.py
+++ crawler/financial/settings.py
@@ -17,18 +17,18 @@ NEWSPIDER_MODULE = 'financial.spiders'
 #USER_AGENT = 'financial (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
--- crawler/financial/spiders/financial.py
+++ crawler/financial/spiders/financial.py
@@ -1,14 +1,51 @@
-from scrapy.spiders import CSVFeedSpider
+import os
+from csv import DictReader
+from scrapy.spiders import Spider
+from scrapy import Request
 
+URL_ENDPOINT = 'https://mops.twse.com.tw/server-java/t164sb01?step=1'
+FILENAME_COMPANY = 'company.csv'
 
-class FinancialSpider(CSVFeedSpider):
+# load company codes scraped from spider "company"
+filepath_company = os.path.join(os.path.dirname(__file__), '../../../data/{}'.format(FILENAME_COMPANY))
+with open(filepath_company) as f:
+    csv_reader = DictReader(f)
+    stock_ids = [row['stock_id'] for row in csv_reader]
+
+class FinancialSpider(Spider):
     name = 'financial'
-    start_urls = [
-        'https://mopsfin.twse.com.tw/opendata/t187ap03_P.csv', # 公開發行公司 https://data.gov.tw/dataset/28567
-    ]
     custom_settings = {
         'LOG_LEVEL': 'INFO'
     }
 
-    def parse_row(self, response, row):
-        pass
+    def start_requests(self):
+        for stock_id in stock_ids:
+
+            yield Request(
+                url=self._gen_request_url(stock_id),
+                meta={'stock_id': stock_id},
+                callback=self.parse,
+            )
+
+    def parse(self, response):
+        self.logger.debug(response.url)
+        filepath = self._gen_financial_webpage_filepath(response.meta['stock_id'])
+        with open(filepath, 'wb') as f:
+            f.write(response.body)
+
+    def _gen_request_url(self, stock_id: str):
+        return '{}&{}'.format(URL_ENDPOINT, self._gen_payload(stock_id))
+
+    def _gen_payload(self, stock_id: str, year='2020', report_id='C'):
+        return (
+            "CO_ID={}&SYEAR={}&SSEASON=4&REPORT_ID={}".format(
+                stock_id,
+                year,
+                report_id
+            )
+        )
+
+    def _gen_financial_webpage_filepath(self, stock_id: str):
+        filename = '{}.html'.format(stock_id)
+        filepath = os.path.join(os.path.dirname(__file__), '../../../data/financial/webpages/{}'.format(filename))
+        return filepath
