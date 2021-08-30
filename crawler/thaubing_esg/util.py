2cfb98f164f75d44a3e3cb68bb8974210befdbff
Hsin <hsin@impactinstitute.com>
2021-08-30 21:49:37 +0200

    move alt zh_name to zh_name

--- crawler/thaubing_esg/util.py
+++ crawler/thaubing_esg/util.py
@@ -2,41 +2,36 @@ accounting_subjects = [
     {
       "key": "total_operating_revenue",
       "eng_name": ["Total operating revenue", "Total revenue"],
-      "name": "營業收入合計",
-      "alt_name": "收入合計"
+      "zh_name": ["營業收入合計", "收入合計"],
     },
     {
       "key": "total_operating_costs",
       "eng_name": ["Total operating costs", "Total costs"],
-      "name": "營業成本合計",
-      "alt_name": "支出合計"
+      "zh_name": ["營業成本合計", "支出合計"],
     },
     {
       "key": "total_operating_expenses",
       "eng_name": ["Total operating expenses", "Total expenses", "Total expenditure and expense"],
-      "name": "營業費用合計"
+      "zh_name": "營業費用合計"
     },
     {
       "key": "net_operating_income",
       "eng_name": "Net operating income (loss)",
-      "name": "營業利益（損失）"
+      "zh_name": "營業利益（損失）"
     },
     {
       "key": "profit_or_loss",
       "eng_name": "Profit (loss)",
-      "name": "本期淨利（淨損）",
-      "alt_name": "本期稅後淨利（淨損）"
+      "zh_name": ["本期淨利（淨損）", "本期稅後淨利（淨損）"],
     },
     {
       "key": "total_comprehensive_income",
       "eng_name": "Total comprehensive income",
-      "name": "本期綜合損益總額",
-      "alt_name": "綜合損益總額"
+      "zh_name": ["本期綜合損益總額", "綜合損益總額"],
     },
     {
       "key": "total_assets",
       "eng_name": "Total assets",
-      "name": "資產總計",
-      "alt_name": "資產總額"
+      "zh_name": ["資產總計", "資產總額"],
      }
 ]
