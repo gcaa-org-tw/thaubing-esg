cd9ff909b28c07304436146bb6ffdd6d56e9e7f1
Hsin <hsin@impactinstitute.com>
2021-08-30 22:57:53 +0200

    update key as en_name

--- crawler/thaubing_esg/util.py
+++ crawler/thaubing_esg/util.py
@@ -1,37 +1,37 @@
 accounting_subjects = [
     {
       "key": "total_operating_revenue",
-      "eng_name": ["Total operating revenue", "Total revenue"],
+      "en_name": ["Total operating revenue", "Total revenue"],
       "zh_name": ["營業收入合計", "收入合計", "收益合計"],
     },
     {
       "key": "total_operating_costs",
-      "eng_name": ["Total operating costs", "Total costs"],
+      "en_name": ["Total operating costs", "Total costs"],
       "zh_name": ["營業成本合計", "支出合計"],
     },
     {
       "key": "total_operating_expenses",
-      "eng_name": ["Total operating expenses", "Total expenses", "Total expenditure and expense"],
+      "en_name": ["Total operating expenses", "Total expenses", "Total expenditure and expense"],
       "zh_name": ["營業費用合計", "支出及費用合計"]
     },
     {
       "key": "net_operating_income",
-      "eng_name": "Net operating income (loss)",
+      "en_name": "Net operating income (loss)",
       "zh_name": ["營業利益（損失）", "營業利益"]
     },
     {
       "key": "profit_or_loss",
-      "eng_name": "Profit (loss)",
+      "en_name": "Profit (loss)",
       "zh_name": ["本期淨利（淨損）", "本期稅後淨利（淨損）"],
     },
     {
       "key": "total_comprehensive_income",
-      "eng_name": "Total comprehensive income",
+      "en_name": "Total comprehensive income",
       "zh_name": ["本期綜合損益總額", "綜合損益總額"],
     },
     {
       "key": "total_assets",
-      "eng_name": "Total assets",
+      "en_name": "Total assets",
       "zh_name": ["資產總計", "資產總額"],
      }
 ]
