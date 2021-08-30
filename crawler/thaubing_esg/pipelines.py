4b96910da04b03d40079854f9884327e412306fa
Hsin <hsin@impactinstitute.com>
2021-08-30 23:49:04 +0200

    sort income.csv by stock_code

--- crawler/thaubing_esg/pipelines.py
+++ crawler/thaubing_esg/pipelines.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from scrapy.exporters import CsvItemExporter
 
 def gen_data_filepath(filename: str):
@@ -36,14 +37,14 @@ class CompanyPipeline:
 class IncomePipeline:
 
     def open_spider(self, spider):
-        filepath = gen_data_filepath('income/income-{}.csv'.format(spider.year))
-        self.file = open(filepath, 'wb')
+        self.filepath = gen_data_filepath('income/income-{}.csv'.format(spider.year))
+        self.file = open(self.filepath, 'wb')
         self.exporter = CsvItemExporter(self.file)
 
         # specifies exported fields and order
         self.exporter.fields_to_export = [
-            'stock_code',
             'year',
+            'stock_code',
             'total_operating_revenue',
             'total_operating_costs',
             'total_operating_expenses',
@@ -56,7 +57,14 @@ class IncomePipeline:
     def close_spider(self, spider):
         self.exporter.finish_exporting()
         self.file.close()
+        spider.logger.info('Sorting csv file by stock_id...')
+        self._csv_sorting_by_stock_id(self.filepath)
 
     def process_item(self, item, spider):
         self.exporter.export_item(item)
         return item
+
+    def _csv_sorting_by_stock_id(self, filepath: str):
+        data = pd.read_csv(filepath)
+        data.sort_values('stock_code')
+        data.to_csv(filepath, index=False)
