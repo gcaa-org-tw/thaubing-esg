f9052491aa3da16ff5f5b243ffd6dab640e4583c
Hsin <44002425+chengh42@users.noreply.github.com>
2021-09-12 10:49:43 +0200

    scrape environmental data (#26)
    * set up epa spider
    
    * fix fieldnames, comments; assign col_id
    
    * order fields_to_export by item field col_id
    
    * fix filepath
    
    * unuse item parsing for now
    
    * add back EpaPipeline
    
    * untrack .csv
    
    * add epa datasets

--- crawler/thaubing_esg/pipelines.py
+++ crawler/thaubing_esg/pipelines.py
@@ -157,3 +157,31 @@ class ShareholdingPipeline:
         data = pd.read_csv(filepath)
         data.sort_values(['year', 'stock_code'], ascending=[False, True], inplace=True)
         data.to_csv(filepath, index=False)
+
+
+class EpaPipeline:
+
+    def open_spider(self, spider):
+        self.filepath = gen_data_filepath(spider.dataset_id + '.csv')
+        self.file = open(self.filepath, 'wb')
+        self.exporter = CsvItemExporter(self.file, encoding='utf-8-sig')
+
+        # # specifies exported fields and order
+        # self.exporter.fields_to_export = spider.fields_to_export
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+        # spider.logger.info('Drop dulicated rows...')
+        # self._csv_drop_duplicates(spider, self.filepath)
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
+
+    # def _csv_drop_duplicates(self, spider, filepath: str):
+    #     data = pd.read_csv(filepath)
+    #     spider.logger.debug('Before dropping: total number of rows = %s', len(data))
+    #     data.drop_duplicates(inplace=True)
+    #     spider.logger.debug('After dropping: total number of rows = %s', len(data))
+    #     data.to_csv(filepath, index=False)
