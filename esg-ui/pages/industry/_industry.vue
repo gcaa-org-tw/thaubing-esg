2acefdfeea8536ad19d440cad94c8ccb17631908
ddio <ddioibb@gmail.com>
2021-10-10 22:02:46 +0800

    feat: apply real data to industry page, #31

--- esg-ui/pages/industry/_industry.vue
+++ esg-ui/pages/industry/_industry.vue
@@ -1,13 +1,32 @@
 <template lang="pug">
-  .industry.mv5.mw8.center
-    h1.b.f1 {{$route.params.industry}}
-    .pa3.ba.mv3
-      .pv1.mb1.bb.b--moon-gray(v-for="(row, i) in stats.body" :key="i")
-        nuxt-link(:to="companyUrl(row.統編)") {{companyAbbr(row.統編)}}
-        span - {{row.年份}} - {{row.項目}}: {{row.數值}} {{row.單位}}
+  .industry.mv5.center
+    select(v-model="industry")
+      option(v-for="opt in industries" :key="opt") {{opt}}
+    table.mv3.ba.b--moon-gray
+      thead
+        th 公司名稱
+        th(v-for="column in perEsgColumns.ALL" :key="column.key") {{column.key}}
+      tr.striped--light-gray(v-for="row in visibleStats" :key="row.company.統編")
+        td
+          nuxt-link(:to="companyUrl(row.company)") {{row.company.公司簡稱}}
+        td(v-for="column in perEsgColumns.ALL" :key="column.key")
+          span {{beautyValue(row, column)}}
 </template>
 <script>
 import { friendlyHeader } from '~/libs/crawlerFriendly'
+import industries from '~/assets/industries.json'
+
+const COLUMNS = [
+  { cat: 'E', subCat: '溫室氣體排放', measure: '範疇一（直接排放）' },
+  { cat: 'E', subCat: '溫室氣體排放', measure: '範疇二（間接排放）' },
+  { cat: 'S', subCat: '員工薪資', measure: '平均薪資' },
+  { cat: 'S', subCat: '員工薪資', measure: '薪資中位數' },
+  { cat: 'G', subCat: '', measure: '資本額' },
+  { cat: 'G', subCat: '收入、成本與淨利', measure: '營業收入' },
+  { cat: 'G', subCat: '收入、成本與淨利', measure: '營業成本' },
+  { cat: 'G', subCat: '收入、成本與淨利', measure: '營業費用' },
+  { cat: 'G', subCat: '收入、成本與淨利', measure: '淨利' }
+]
 
 export default {
   async asyncData ({ $content, params, redirect }) {
@@ -22,29 +41,93 @@ export default {
       redirect('/')
     }
   },
+  data () {
+    return {
+      industry: this.$route.params.industry,
+      order: '溫室氣體排放-範疇一（直接排放）',
+      isAsc: false
+    }
+  },
   head: friendlyHeader({
     title () {
       return this.$route.params.industry
     }
   }),
-  methods: {
-    getCompany (id) {
-      return this.companyList.body.find(item => item.統編 === id) || {}
+  computed: {
+    year () {
+      return this.$route.query.year || '2019'
     },
-    companyAbbr (id) {
-      const meta = this.getCompany(id)
-      return meta.公司簡稱 || ''
+    industries () {
+      return industries
     },
-    companyUrl (id) {
-      const abbr = this.companyAbbr(id)
-      if (!abbr) {
-        return '#'
+    perEsgColumns () {
+      const columnMap = { E: [], S: [], G: [] }
+      COLUMNS.forEach((column) => {
+        columnMap[column.cat].push({
+          key: `${column.subCat}-${column.measure}`,
+          meta: column
+        })
+      })
+      return {
+        ...columnMap,
+        ALL: [...columnMap.E, ...columnMap.S, ...columnMap.G]
       }
-      return `/company/${abbr}`
+    },
+    companyStats () {
+      const companyMap = this.companyList.body.reduce((map, company) => {
+        if (company.自訂產業別 !== this.$route.params.industry) {
+          return map
+        }
+        map[company.統編] = {
+          company,
+          stats: {}
+        }
+        return map
+      }, {})
+      this.stats.body.forEach((stat) => {
+        const company = companyMap[stat.統編]
+        if (!company || stat.年份 !== this.year) {
+          return
+        }
+        stat.value = Number.parseFloat(stat.數值)
+        company.stats[`${stat.子分類}-${stat.項目}`] = stat
+      })
+      return Object.values(companyMap)
+    },
+    visibleStats () {
+      return [...this.companyStats].sort((a, b) => {
+        if (this.isAsc) {
+          return a.stats[this.order] - b.stats[this.order]
+        }
+        return b.stats[this.order] - a.stats[this.order]
+      })
+    }
+  },
+  watch: {
+    industry () {
+      this.$router.push(`/industry/${this.industry}`)
+    }
+  },
+  methods: {
+    companyUrl (company) {
+      return `/company/${company.公司簡稱}`
+    },
+    beautyValue (row, column) {
+      if (!(column.key in row.stats)) {
+        return '-'
+      }
+      const field = row.stats[column.key]
+      if (!isNaN(field.value)) {
+        const value = Math.round(field.value * 100) / 100
+        return `${value.toLocaleString()} (${field.單位})`
+      }
+      return `${field.數值} (${field.單位})`
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-.industry {}
+.industry {
+  max-width: calc(100vw - 10rem);
+}
 </style>
