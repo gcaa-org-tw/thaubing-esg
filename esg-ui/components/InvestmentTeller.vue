dd79e4c2e6f3443aaf47b5303317272a56d8332d
ddio <ddioibb@gmail.com>
2021-11-11 16:37:51 +0800

    feat: add fund compare, #61

---
+++ esg-ui/components/InvestmentTeller.vue
@@ -0,0 +1,250 @@
+<template lang="pug">
+  .invTeller(:style="invStyle")
+    .invTeller__measureHead
+      button.invTeller__switch.bg-transparent.f3.fw6.mb2.pointer.db.pa0(
+        v-for="measure in orderOptions"
+        @click="switchOrder"
+        :class="[measure === orderBy ? 'o-100' : 'o-50']"
+      ) {{measure}}
+    .invTeller__fundHead.black(v-for="fund in fundList")
+      .fw6 {{fund.cat}}
+      .fw4(v-for="line in fundName(fund)") {{line}}
+    template(v-for="stat in sortedStats")
+      .invTeller__stat.bg-white.br2.br--left
+        .invTeller__statVal.br2.h-100.ml-auto.fw6.pr1.flex.items-center.justify-end(:class="barClass(stat)" :style="barStyle(stat)") {{printValue(stat)}}
+      .invTeller__company.bg-white.flex.items-center
+        | {{stat.company.公司簡稱}}
+      .invTeller__fundFlag.bg-white.flex.items-center(v-for="fund in fundList")
+        .invTeller__flag(v-if="stat.X[fund.name]" :class="flagClass(stat)") T
+</template>
+<script>
+const TARGET_MEASURES = ['碳密集度', '能源密集度']
+export default {
+  props: {
+    stats: {
+      type: Array,
+      required: true
+    },
+    companyMap: {
+      type: Object,
+      required: true
+    },
+    year: {
+      type: String,
+      default: '2019'
+    }
+  },
+  data () {
+    return {
+      orderBy: TARGET_MEASURES[1]
+    }
+  },
+  computed: {
+    invStyle () {
+      return {
+        gridTemplateColumns: `1.5fr 4rem repeat(${this.fundList.length}, 1fr)`
+      }
+    },
+    curMaxStatVal () {
+      return this.sortedStats[0].E[this.orderBy]
+    },
+    orderOptions () {
+      return TARGET_MEASURES
+    },
+    fundList () {
+      const list = [...new Set(
+        this.investmentDataPerCompany.map(row => Object.keys(row.X)).flat()
+      )]
+      return list.map((fund) => {
+        if (fund.startsWith('中國信託')) {
+          return {
+            cat: '中國信託',
+            name: fund
+          }
+        }
+        return {
+          cat: fund.slice(0, 2),
+          name: fund
+        }
+      })
+    },
+    investmentDataPerCompany () {
+      const companyMap = this.stats.reduce((ret, row) => {
+        const id = row.統編
+        const company = this.companyMap[id]
+        if (row.年份 !== this.year || !company) {
+          return ret
+        }
+        if (row.分類 === 'X' || (row.來自公司報告 && TARGET_MEASURES.includes(row.項目))) {
+          if (!ret[id]) {
+            ret[id] = { X: {}, E: {}, company }
+          }
+          let value = row.數值
+          if (row.分類 === 'E') {
+            value = Number.parseFloat(value)
+          }
+          ret[id][row.分類][row.項目] = value
+        }
+        return ret
+      }, {})
+
+      const quartileMap = TARGET_MEASURES.reduce((map, measure) => {
+        map[measure] = this.findQuartileValue(companyMap, measure)
+        return map
+      }, {})
+
+      return Object.values(companyMap).map((row) => {
+        row.color = {}
+        TARGET_MEASURES.forEach((measure) => {
+          const value = row.E[measure]
+          if (Number.isNaN(value) || value === undefined) {
+            row.E[measure] = quartileMap[measure].thridForth
+            row.color[measure] = 'unknown'
+          } else if (value >= quartileMap[measure].thridForth) {
+            row.color[measure] = 'red'
+          } else if (value >= quartileMap[measure].oneForth) {
+            row.color[measure] = 'yellow'
+          } else {
+            row.color[measure] = 'green'
+          }
+        })
+        return row
+      })
+    },
+    sortedStats () {
+      const measure = this.orderBy
+      return [...this.investmentDataPerCompany].sort((a, b) => {
+        return b.E[measure] - a.E[measure]
+      })
+    }
+  },
+  methods: {
+    printValue (stat) {
+      if (stat.color[this.orderBy] === 'unknown') {
+        return '?'
+      }
+      return stat.E[this.orderBy].toFixed(2)
+    },
+    switchOrder () {
+      this.orderBy = TARGET_MEASURES.find(haystack => haystack !== this.orderBy)
+    },
+    fundName (fund) {
+      const len = Math.round(fund.name.length / 2)
+      return [
+        fund.name.slice(0, len),
+        fund.name.slice(len)
+      ]
+    },
+    barClass (stat) {
+      return [`invTeller__statVal--${stat.color[this.orderBy]}`]
+    },
+    barStyle (stat) {
+      return {
+        width: `${stat.E[this.orderBy] * 100 / this.curMaxStatVal}%`
+      }
+    },
+    flagClass (stat) {
+      return [`invTeller__flag--${stat.color[this.orderBy]}`]
+    },
+    findQuartileValue (companyMap, measure) {
+      const sortedValues = Object.values(companyMap)
+        .filter((row) => {
+          return !Number.isNaN(row.E[measure])
+        })
+        .map(row => row.E[measure])
+        .sort((a, b) => a - b)
+
+      const len = sortedValues.length
+      const ret = {
+        oneForth: 0,
+        thridForth: 0
+      }
+      if (len === 1) {
+        return {
+          oneForth: sortedValues[0],
+          thridForth: sortedValues[0]
+        }
+      } else {
+        const oneForthLen = Math.round(len / 4)
+        ret.oneForth = (sortedValues[oneForthLen - 1] + sortedValues[oneForthLen]) / 2
+        ret.thridForth = (sortedValues[len - oneForthLen] + sortedValues[len - oneForthLen - 1]) / 2
+      }
+      return ret
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+$red: #E24243;
+$yellow: #FACB3D;
+$green: #6FAC45;
+$unknown: #c1c1c1;
+
+.invTeller {
+  display: grid;
+  row-gap: 0.375rem;
+  max-width: 80rem;
+
+  &__measureHead {
+    grid-column: 1 / span 2;
+    padding: 0.375rem 0.625rem;
+  }
+  &__fundHead {
+    padding: 0.375rem 0.625rem;
+  }
+
+  &__stat {
+    background: white;
+    height: 2rem;
+  }
+  &__statVal {
+    background: $unknown;
+    color: black;
+    &--red {
+      background: $red;
+      color: white;
+    }
+    &--green {
+      background: $green;
+    }
+    &--yellow {
+      background: $yellow;
+    }
+  }
+  &__company {
+    padding: 0 0.5rem;
+  }
+  &__fundFlag {
+    padding: 0 0.625rem;
+    &:last-child {
+      border-top-right-radius: 0.25rem;
+      border-bottom-right-radius: 0.25rem;
+    }
+  }
+  &__flag {
+    color: $unknown;
+    background: $unknown;
+    width: 1.25rem;
+    height: 1.25rem;
+    border: 2px solid rgba(0, 0, 0, 0.1);
+    border-radius: 100%;
+    &--red {
+      color: $red;
+      background: $red;
+    }
+    &--yellow {
+      color: $yellow;
+      background: $yellow;
+    }
+    &--green {
+      color: $green;
+      background: $green;
+    }
+  }
+  &__switch {
+    line-height: 1.5;
+    color: #35811C;
+    border: none;
+  }
+}
+</style>
