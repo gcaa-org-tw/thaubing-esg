ff7ac71cfb6ef2e9d2da8b203249a447d777d8d9
ddio <ddio@users.noreply.github.com>
2022-04-29 09:45:53 +0000

    deploy: dbbd015cac975a4fca85b5f87b05eda2a6adecea

--- _nuxt/01a1ca5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{452:function(e,n,t){"use strict";t.r(n);var r={middleware:function(e){return(0,e.redirect)("/industry/石化業")}},c=t(47),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"index"})}),[],!1,null,null,null);n.default=component.exports}}]);
