babc19462841f020791ff2287b2f315b3149cb8b
ddio <ddio@users.noreply.github.com>
2022-05-17 09:25:37 +0000

    deploy: 21e32b50fa4e822d6e479f6ca6257cbfb1391c97

--- _nuxt/static/1652513024/company/23504361/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652513024",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcompany\u002F23504361",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"21fc3998"}}}}(null));
--- _nuxt/static/1652513024/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652513024",layout:"default",error:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"21fc3998"}}}}(null,"\u002F"));
