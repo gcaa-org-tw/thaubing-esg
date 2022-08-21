a8f3affeca3d954aa8ce8c3e6c64e37683da96dc
ddio <ddio@users.noreply.github.com>
2022-08-21 08:12:45 +0000

    deploy: c72d843c535d1fb6a8e4c4c5e6f3a957ec7554e2

--- _nuxt/static/1661057958/net-zero/01/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661057958",layout:"default",error:a,state:{isGuanshiyinn:false,company:{}},serverRendered:true,routePath:"\u002Fnet-zero\u002F01",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"019269e8"}}}}(null));
---
+++ _nuxt/static/1661068879/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661068879",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"59dd4b35"}}}}(null,"\u002F"));
