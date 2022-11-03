b3091f4925839e7e4571120ab81a8d56951ef829
ddio <ddio@users.noreply.github.com>
2022-11-03 07:32:58 +0000

    deploy: 5d36b9e74cdeea7f1f547cb21f3fc8764ff080db

---
+++ _nuxt/static/1667460077/industry/06/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667460077",layout:"default",error:a,state:{isGuanshiyinn:false,company:{}},serverRendered:true,routePath:"\u002Findustry\u002F06",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6c2eb9db"}}}}(null));
---
+++ _nuxt/static/1667460077/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1667460077",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6c2eb9db"}}}}(null,"\u002F"));
