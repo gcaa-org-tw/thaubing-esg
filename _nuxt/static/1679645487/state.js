3e3b96aa2dd7b7da379c69595e95f7bf920270e9
ddio <ddio@users.noreply.github.com>
2023-03-24 08:27:15 +0000

    deploy: b334406914a2b646b623cf0b02fef233839f9c73

--- _nuxt/static/1679645487/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1679645487",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9123f2fb"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1679645726/company/39660325/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1679645726",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcompany\u002F39660325",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"692c2a8d"}}}}(null));
