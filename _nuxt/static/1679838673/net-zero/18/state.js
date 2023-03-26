d2f83f45d79277c13ff7c7647f3603a037fcc069
ddio <ddio@users.noreply.github.com>
2023-03-26 14:07:03 +0000

    deploy: 0c72b2519c39addd1d62aa81ed5282b173735de9

---
+++ _nuxt/static/1679838673/net-zero/18/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1679838673",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fnet-zero\u002F18",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c013c0b7"}}}}(null));
