d71bb7ee7e864118c9c8451c076c121738ae235e
qyke <qyke@users.noreply.github.com>
2023-06-16 10:46:07 +0000

    deploy: b93ba84df0db3aa35039aecca3ddbcf54b98a9b1

--- _nuxt/static/1686827869/industry/01/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1686827869",layout:"default",error:a,state:{isGuanshiyinn:false,company:{}},serverRendered:true,routePath:"\u002Findustry\u002F01",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b46ce1b"}}}}(null));
--- _nuxt/static/1686827869/net-zero/30/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1686827869",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fnet-zero\u002F30",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7b46ce1b"}}}}(null));
