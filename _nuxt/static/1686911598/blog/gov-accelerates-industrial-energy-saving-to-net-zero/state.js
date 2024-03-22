7234143ac252c30cc5bd9bdf29d6e9660a9fd762
ddio <ddio@users.noreply.github.com>
2024-03-22 10:16:25 +0000

    deploy: c8654b1cdd5c56f03a44bea97742f358a1cff232

--- _nuxt/static/1686911598/blog/gov-accelerates-industrial-energy-saving-to-net-zero/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1686911598",layout:"empty",error:a,state:{isGuanshiyinn:false,company:{}},serverRendered:true,routePath:"\u002Fblog\u002Fgov-accelerates-industrial-energy-saving-to-net-zero",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"890337b3"}}}}(null));
---
+++ _nuxt/static/1711102045/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1711102045",layout:"empty",error:a,state:{isGuanshiyinn:false,company:{}},serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cce551ee"}}}}(null));
