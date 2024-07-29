ffd99784a5053d3774f67f6e2d6c56d60bc2ec9d
ddio <ddio@users.noreply.github.com>
2024-07-29 07:25:12 +0000

    deploy: cdf30fb91c61acbc6f052a5232352d6e5525c2ff

--- _nuxt/static/1721652666/landing/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1721652666",layout:"empty",error:a,state:{isGuanshiyinn:false,company:{}},serverRendered:true,routePath:"\u002Flanding",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a1bed05c"}}}}(null));
---
+++ _nuxt/static/1722237368/net-zero/14/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1722237368",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fnet-zero\u002F14",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"775b02b4"}}}}(null));
